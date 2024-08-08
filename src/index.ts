28dd2f07b31fa578a0383fec18cd97630561848b
Caio Fuccio <caio.fuccio@gmail.com>
2024-08-07 22:51:59 -0300

    feat: initial configurations of express server

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import cors from 'cors';
+import express from 'express';
+import dotenv from 'dotenv';
+
+const main = async (port: number) => {
+    try {
+        const app = express();
+        app.use(express.urlencoded({ extended: true }));
+        app.use(express.json());
+        app.use(cors());
+
+        app.listen(port || 8080, () =>
+            console.log(`Server running on port ${port}`),
+        );
+    } catch (error) {
+        throw new Error('Unable to start the server');
+    }
+};
+
+(async () => {
+    dotenv.config();
+    const { PORT } = process.env;
+
+    await main(Number(PORT));
+})();
